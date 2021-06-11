ea37a8ca0d0706f349a46fb63eb0d3b0c648f291
keithcharleschan <keithcharleschan@gmail.com>
2021-06-12 01:15:53 +0800

    gapi with firebase login

--- src/components/Navbar Pages/Dashboard.js
+++ src/components/Navbar Pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Card } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { useAuth } from "../../contexts/AuthContext";
 import NavigationBar from "./NavigationBar";
 
@@ -19,41 +19,6 @@ export default function Dashboard() {
             <p className="d-flex align-items-center justify-content-center">
               {currentUser.displayName}
             </p>
-            {/*
-            <Card>
-              <Card.Body className="text-dark h6">
-                <small>
-                  <p>Hi, thanks for visiting our website!</p>
-                  <p>Currently:</p>
-                  <ul>
-                    <li>
-                      The Channels/Calendar/Contact Us Pages have yet to be
-                      build.
-                    </li>
-                  </ul>
-                  <p>Existing features:</p>
-                  <ul>
-                    <li className="mb-1">
-                      <strong>Navigation Bar</strong> that directs you to the
-                      different pages (Clicking on icon directs to home page)
-                    </li>
-                    <li className="mb-1">
-                      You can click on the <strong>profile tab</strong> in the
-                      navbar to update your email and password!
-                    </li>
-                    <li className="mb-1">
-                      You can see the{" "}
-                      <strong>
-                        existing logged in email in the top right corner
-                      </strong>{" "}
-                      and a <strong>logout button</strong> to sign you out!
-                    </li>
-                    <li>Sign up, Login, Forget/Reset Password features!</li>
-                  </ul>
-                </small>
-              </Card.Body>
-            </Card>
-            */}
           </div>
         </Container>
       </div>
