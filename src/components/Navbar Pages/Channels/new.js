e27fffb7c2bd4c765bdf54f3d09b694e822a331e
denshem10 <denshem10@gmail.com>
2021-06-19 20:33:08 +0800

    Updated FAQ page

---
+++ src/components/Navbar Pages/Channels/new.js
@@ -0,0 +1,21 @@
+import React, { useRef, useState, useEffect } from "react";
+import {
+  Container,
+  Form,
+  Card,
+  Button,
+  Alert,
+  Row,
+  Badge,
+  Col,
+} from "react-bootstrap";
+import NavigationBar from "../NavigationBar/NavigationBar";
+import { firebase } from "@firebase/app";
+import Popup from "./Popup";
+import InviteList from "./InviteList";
+import dateRange from "./dateRange";
+import Channels from "./Channels";
+
+export default function New() { 
+  
+}
